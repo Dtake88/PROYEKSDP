4fef410856f18062070c278b38661d13b52a94d7
Dtake88 <56423564+Dtake88@users.noreply.github.com>
2020-12-14 13:16:18 +0700

    done

--- app/Http/Controllers/Database.php
+++ app/Http/Controllers/Database.php
@@ -876,7 +876,6 @@ class Database extends Controller
 
     public function downloadToa($namafile)
     {
-        dd($namafile);
         return Storage::disk('local')->download("fileToa/".$namafile);
     }
 
